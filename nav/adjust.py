bdecb70e7f38b2cbcebef05345a229074b995105
wfhendrix11 <wfhendrix11@gmail.com>
2019-02-18 12:41:33 -0600

    Red Light |

--- nav/adjust.py
+++ nav/adjust.py
@@ -1,4 +1,10 @@
+import math
 
+DEFAULT_TEMPERATURE = 72
+DEFAULT_PRESSURE = 1010
+DEFAULT_HEIGHT = 0
+DEFAULT_HORIZON = "natural"
+MANDATORY_PARAMETERS = ['observation']
 
 def adjust(values = None):
     return values
--- nav/test/adjustTest.py
+++ nav/test/adjustTest.py
@@ -4,10 +4,70 @@ import nav.adjust as nav
 class adjustTest(unittest.TestCase):
 
     def setUp(self):
-        pass
+    	self.inputDictionary = {}
+        self.errorKey = "error"
+        self.solutionKey = "probability"
+        self.BX_PATH = '/nav?'
+        self.BX_PORT = 5000
+        self.BX_URL = 'localhost'
 
     def tearDown(self):
-        pass
+        self.inputDictionary = {}
 
-    def testName(self):
-        pass  #<--  unit tests for adjust go here
+    def setParm(self, key, value):
+        self.inputDictionary[key] = value
+
+    def microservice(self):
+        try:
+            theParm = urlencode(self.inputDictionary)
+            theConnection = httplib.HTTPConnection(self.BX_URL, self.BX_PORT)
+            theConnection.request("GET", self.BX_PATH + theParm)
+            theStringResponse = theConnection.getresponse().read()
+            return theStringResponse
+        except Exception as e:
+            return "error encountered during transaction"
+
+    def string2dict(self, httpResponse):
+        '''Convert JSON string to dictionary'''
+        result = {}
+        try:
+            jsonString = httpResponse.replace("'", "\"")
+            unicodeDictionary = json.loads(jsonString)
+            for element in unicodeDictionary:
+                if(isinstance(unicodeDictionary[element],unicode)):
+                    result[str(element)] = str(unicodeDictionary[element])
+                else:
+                    result[str(element)] = unicodeDictionary[element]
+        except Exception as e:
+            result['diagnostic'] = str(e)
+        return result    
+
+# -----------------------------------------------------------------------
+    # ---- Acceptance Tests
+    # 100 adjust operation
+    #   Happy path analysis:
+    #        values:      mandatory
+    #                     dictionary
+    #                     Operations:   {'op':'adjust'}
+    #                      
+    #   Sad path analysis:
+    #        values:
+    #                     no op specified             values={}
+    #                        -- return {'error':'no op  is specified'}
+    #                     contain 'error' as a key      values={5d04.9', 'height': '6.0', 'pressure': '1010',
+    #                                                           'horizon': 'artificial', 'temperature': '72'
+    #                                                           'error':'no op is specified'}'
+    #                        -- return values without error as a key and without its values
+    #                     not-dictionary                values=42
+    #                        -- return {'error':'parameter is not a dictionary'}
+    #                     not legal operation           values={'op': 'unknown'}
+    #                        -- return {'error':'op is not a legal operation'}
+    #                     missing dictionary            dispatch()
+    #                        -- return {'error':'dictionary is missing'}
+    # Happy path
+
+    def test100_10(self):
+    	self.setParm('op','adjust')
+    	result = self.microservice()
+        resultDictionary = self.string2dict(result)
+        self.assertDictEqual(self.inputDictionary, resultDictionary)
