6fa3ff19508a093515445dbb37514170d99f48fb
wfhendrix11 <wfhendrix11@gmail.com>
2019-03-03 10:10:44 -0600

    added predict test file

---
+++ nav/test/predictTest.py
@@ -0,0 +1,85 @@
+'''
+    Created on February 18, 2018
+
+    @author: William Hendrix
+'''
+
+import unittest
+import nav.predict as nav
+import httplib
+from urllib import urlencode
+import json
+
+class predictTest(unittest.TestCase):
+
+    def setUp(self):
+        self.inputDictionary = {}
+        self.errorKey = "error"
+        self.solutionKey = "probability"
+        self.BX_PATH = '/nav?'
+        self.BX_PORT = 5000
+        self.BX_URL = 'localhost'
+
+    def tearDown(self):
+        self.inputDictionary = {}
+
+    def setParm(self, key, value):
+        self.inputDictionary[key] = value
+
+    def microservice(self):
+        try:
+            theParm = urlencode(self.inputDictionary)
+            theConnection = httplib.HTTPConnection(self.BX_URL, self.BX_PORT)
+            theConnection.request("GET", self.BX_PATH + theParm)
+            theStringResponse = theConnection.getresponse().read()
+            return theStringResponse
+        except Exception as e:
+            return "error encountered during transaction"
+
+    def string2dict(self, httpResponse):
+        '''Convert JSON string to dictionary'''
+        result = {}
+        try:
+            jsonString = httpResponse.replace("'", "\"")
+            unicodeDictionary = json.loads(jsonString)
+            for element in unicodeDictionary:
+                if(isinstance(unicodeDictionary[element],unicode)):
+                    result[str(element)] = str(unicodeDictionary[element])
+                else:
+                    result[str(element)] = unicodeDictionary[element]
+        except Exception as e:
+            result['diagnostic'] = str(e)
+        return result    
+
+# -----------------------------------------------------------------------
+    # ---- Acceptance Tests
+    # 100 predict operation
+    #   Happy path analysis:
+    #        values:      mandatory
+    #                     dictionary
+    #                     Operations:   {'op':'predict'}
+    #                      
+    #   Sad path analysis:
+    #        values:
+    #                     no op specified             values={}
+    #                        -- return {'error':'no op  is specified'}
+    #                     contain 'error' as a key      values={5d04.9', 'height': '6.0', 'pressure': '1010',
+    #                                                           'horizon': 'artificial', 'temperature': '72'
+    #                                                           'error':'no op is specified'}'
+    #                        -- return values without error as a key and without its values
+    #                     not-dictionary                values=42
+    #                        -- return {'error':'parameter is not a dictionary'}
+    #                     not legal operation           values={'op': 'unknown'}
+    #                        -- return {'error':'op is not a legal operation'}
+    #                     missing dictionary            dispatch()
+    #                        -- return {'error':'dictionary is missing'}
+    
+    # --------------------- Happy path ---------------------
+
+    def test100_010ShouldReturnChangedValuesWithOperationPredict(self):   
+        # Arrange
+          
+        # Act
+          
+        # Assert
+        pass
