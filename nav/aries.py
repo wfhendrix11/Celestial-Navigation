014e7e805278a52469134e442f19a6cd7223a5e8
wfhendrix11 <wfhendrix11@gmail.com>
2019-03-06 17:36:53 -0600

    Red Light |

--- nav/aries.py
+++ nav/aries.py
@@ -11,11 +11,31 @@ from nav.angle import Angle
 class Aries:
     
     '''
-    getGreenwich
+    getGreenwichHourAngle
         :params year, month, day, hour, minute, seconds
         :return an adjusted dictionary of values
     '''
     @classmethod 
     def getGreenwichHourAngle(year, month, day, hour, minute, second):
+        
+        referenceDatetime = str(year) + ",01,01,00,00,00"
+        observationDatetime = str(year) + ',' + str(month) + ',' + str(day) + ','
+        observationDatetime += str(hour) + ',' + str(minute) + ',' + str(second)
+        
+        observationDatetime = datetime.strptime(observationDatetime, '%Y,%m,%d,%H,%M,%S')
+        referenceDatetime = datetime.strptime(referenceDatetime, '%Y,%m,%d,%H,%M,%S')
+        secondsSinceReference = (observationDatetime - referenceDatetime).total_seconds()
+        
+        relativePrimeMeridian = Aries.getPrimeMeridian(year)
+        
+    '''
+    getPrimeMeridian
+        :params year
+        :return total progression
+    '''    
+    @classmethod
+    def getPrimeMeridian(cls, year):
+    
+        return 0
         
         
