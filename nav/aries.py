60750480902727f7656321c7770ec8dca1964f4c
wfhendrix11 <wfhendrix11@gmail.com>
2019-03-06 17:48:56 -0600

    Red Light |

--- nav/aries.py
+++ nav/aries.py
@@ -27,6 +27,9 @@ class Aries:
         secondsSinceReference = (observationDatetime - referenceDatetime).total_seconds()
         
         relativePrimeMeridian = Aries.getPrimeMeridian(year)
+        earthRotation = Aries.getEarthRotationSinceObservation(secondsSinceReference)
+        
+        return Angle.add(relativePrimeMeridian, earthRotation)
         
     '''
     getPrimeMeridian
@@ -35,7 +38,30 @@ class Aries:
     '''    
     @classmethod
     def getPrimeMeridian(cls, year):
+        
+        referenceRotation = Angle.stringToAngle("100d42.6")
+        yearlyGHADecrement = Angle.stringToAngle("-0d14.32")
+        deltaYear = year - 2001
+        cumulativeProgression = Angle.multiply(yearlyGHADecrement, deltaYear)
+        
+        dailyRotation = Angle.stringToAngle("0d59.0")
+        leapYears = math.floor((year - 2001) / 4)
+        leapProgression = Angle.multiply(dailyRotation, leapYears)
+        
+        totalProgression = Angle.add(referenceRotation, cumulativeProgression)
+        totalProgression = Angle.add(totalProgression, leapProgression)
+        
+        return totalProgression
     
-        return 0
+    '''
+    getEarthRotationSinceObservation
+        :params elapsed seconds
+        :return total rotation
+    '''
+    @classmethod
+    def getEarthRotationSinceObservation(cls, elapsedSeconds):
+        rotation = round(elapsedSeconds)
+        
+        return Angle.decimalToAngle(rotation)
         
         
