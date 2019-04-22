21628fb4e30d5fb65ce61adf6592cb5303f8b13d
wfhendrix11 <wfhendrix11@gmail.com>
2019-04-22 18:06:24 -0500

    initial commit

---
+++ nav/locate.py
@@ -0,0 +1,14 @@
+'''
+    Created on April 22, 2019
+
+    @author: William Hendrix
+'''
+import math
+
+'''
+    locate
+        :param a dictionary of values.
+        :return the location. 
+'''      
+def locate(values = None):
+    return 0
